ac2d0937393d8b557b77d5ec50424af649162e95
Kalkidan Amare <kalkidanamare11a@gmail.com>
2024-02-20 05:02:05 +0300

    Add solution for Reverse String

--- leetcode/reverse-string.py
+++ leetcode/reverse-string.py
@@ -3,6 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s.reverse()
-        return s
+        #s.reverse()
+        #s1=list(s)
+        start, end=0, len(s)-1
+        while start < end:
+            s[start],s[end] = s[end],s[start]
+            start += 1
+            end -= 1
         
